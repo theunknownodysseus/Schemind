3893350a14fe913ba7702a0f1355dc1a4726a605
theunknownodysseus <varunify.777@gmail.com>
2025-07-21 14:10:19 +0530

    bg change

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,7 +4,7 @@ import { Brain, BookOpen, Trophy, Rocket, Users, Star, ChevronDown, Zap, Globe, 
 import { Link } from 'react-router-dom';
 import ChatBot from '../components/ChatBot';
 import { useChat } from '../context/ChatContext';
-import DotGrid from '../components/DotGrid';
+import Hyperspeed from '../components/Hyperspeed';
 
 const Home = () => {
   const controls = useAnimation();
@@ -47,17 +47,46 @@ const Home = () => {
       {/* DotGrid Background - fills entire section */}
       
 <div style={{ width: '100%', height: '100%', position: 'absolute'}}>
-  <DotGrid
-    dotSize={4}
-    gap={15}
-    baseColor="#6759a2ff"
-    activeColor="#5227FF"
-    proximity={120}
-    shockRadius={250}
-    shockStrength={5}
-    resistance={750}
-    returnDuration={1.5}
-  />
+  
+<Hyperspeed
+  effectOptions={{
+    onSpeedUp: () => { },
+    onSlowDown: () => { },
+    distortion: 'turbulentDistortion',
+    length: 400,
+    roadWidth: 10,
+    islandWidth: 2,
+    lanesPerRoad: 4,
+    fov: 90,
+    fovSpeedUp: 150,
+    speedUp: 2,
+    carLightsFade: 0.4,
+    totalSideLightSticks: 20,
+    lightPairsPerRoadWay: 40,
+    shoulderLinesWidthPercentage: 0.05,
+    brokenLinesWidthPercentage: 0.1,
+    brokenLinesLengthPercentage: 0.5,
+    lightStickWidth: [0.12, 0.5],
+    lightStickHeight: [1.3, 1.7],
+    movingAwaySpeed: [60, 80],
+    movingCloserSpeed: [-120, -160],
+    carLightsLength: [400 * 0.03, 400 * 0.2],
+    carLightsRadius: [0.05, 0.14],
+    carWidthPercentage: [0.3, 0.5],
+    carShiftX: [-0.8, 0.8],
+    carFloorSeparation: [0, 5],
+    colors: {
+      roadColor: 0x080808,
+      islandColor: 0x0a0a0a,
+      background: 0x000000,
+      shoulderLines: 0xFFFFFF,
+      brokenLines: 0xFFFFFF,
+      leftCars: [0xD856BF, 0x6750A2, 0xC247AC],
+      rightCars: [0x03B3C3, 0x0E5EA5, 0x324555],
+      sticks: 0x03B3C3,
+    }
+  }}
+/>
 </div>
       
       {/* Content - positioned above DotGrid */}
